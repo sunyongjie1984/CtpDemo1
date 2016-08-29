2431d5a1df38ecfaadc5388a517a7b49b4ff3792
sunyongjie <sunyongjie@bogon.(none)>
2016-08-30 06:35:48 +0800

    modify

--- CtpDemo1/main.cpp
+++ CtpDemo1/main.cpp
@@ -4,9 +4,10 @@
 // UserApi对象
 CThostFtdcTraderApi* pUserApi;
 
-char FRONT_ADDR[] = "tcp://180.168.146.187:10000";     // 前置地址
+// char FRONT_ADDR[] = "tcp://180.168.146.187:10000";     // 前置地址
+char FRONT_ADDR[] = "tcp://180.168.146.187:10030";     // 前置地址
 TThostFtdcBrokerIDType BROKER_ID = "9999";             // 经纪公司代码
-TThostFtdcInvestorIDType INVESTOR_ID = "009637";       // 投资者代码
+TThostFtdcInvestorIDType INVESTOR_ID = "069637";       // 投资者代码
 TThostFtdcPasswordType PASSWORD = "sim0594184520";     // 用户密码
 
 TThostFtdcInstrumentIDType INSTRUMENT_ID = "cu0909";   // 合约代码
